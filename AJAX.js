93b1841d844a5dd46d3340c3679c11bb872bad09
Beumsk <beumieremy@gmail.com>
2017-02-22 10:15:02 +0100

    better indentation

--- AJAX.js
+++ AJAX.js
@@ -7,20 +7,20 @@ Asynchronous JAvascript and Xml
 // AJAX GET func
 // target URL and callback func as parameters (callback runs if success)
 function ajaxGet(url, callback) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            // callback with request answer
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Network error with URL : " + url);
-    });
-    req.send(null);
+	var req = new XMLHttpRequest();
+	req.open("GET", url);
+	req.addEventListener("load", function () {
+		if (req.status >= 200 && req.status < 400) {
+			// callback with request answer
+			callback(req.responseText);
+		} else {
+			console.error(req.status + " " + req.statusText + " " + url);
+		}
+	});
+	req.addEventListener("error", function () {
+		console.error("Network error with URL : " + url);
+	});
+	req.send(null);
 }
 
 
@@ -29,24 +29,24 @@ function ajaxGet(url, callback) {
 // AJAX POST func
 // target URL, data to send and callback func as parameters (callback runs if success)
 function ajaxPost(url, data, callback, isJson) { // isJson checks if data is json format
-    var req = new XMLHttpRequest();
-    req.open("POST", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
-            // callback with request answer
-            callback(req.responseText);
-        } else {
-            console.error(req.status + " " + req.statusText + " " + url);
-        }
-    });
-    req.addEventListener("error", function () {
-        console.error("Network error with URL : " + url);
-    });
-    if (isJson) {
-        // request content is JSON
-        req.setRequestHeader("Content-Type", "application/json");
-        // turns data into JSON before sending
-        data = JSON.stringify(data);
-    }
-    req.send(data);
+	var req = new XMLHttpRequest();
+	req.open("POST", url);
+	req.addEventListener("load", function () {
+		if (req.status >= 200 && req.status < 400) {
+			// callback with request answer
+			callback(req.responseText);
+		} else {
+			console.error(req.status + " " + req.statusText + " " + url);
+		}
+	});
+	req.addEventListener("error", function () {
+		console.error("Network error with URL : " + url);
+	});
+	if (isJson) {
+		// request content is JSON
+		req.setRequestHeader("Content-Type", "application/json");
+		// turns data into JSON before sending
+		data = JSON.stringify(data);
+	}
+	req.send(data);
 }
