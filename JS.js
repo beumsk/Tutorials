bd44bf35cf8605dc666cdb7a0fe0862651aadec4
Rémy Beumier <beumieremy@gmail.com>
2019-09-27 10:41:49 +0200

    recursion added

--- JS.js
+++ JS.js
@@ -705,6 +705,16 @@
 
 
 
+  // recursion
+  function countDownFrom(number) {
+    if (number === 0) { return; }
+    console.log(number);
+    countDownFrom(number - 1);
+  }
+  countDownFrom(5); // 5 4 3 2 1
+
+
+
 
 // FUNCTIONS
 // A JavaScript function is a block of code designed to perform a particular task. You need to invoke it
