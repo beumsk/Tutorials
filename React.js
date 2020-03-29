04518a26a215d75ad6fdd28400db40381bc7009a
Rémy Beumier <beumieremy@gmail.com>
2020-03-29 23:49:41 +0200

    react started

---
+++ React.js
@@ -0,0 +1,92 @@
+// REACT - Javascript Framework - by Beumsk
+
+
+
+/**/
+
+
+// write JSX elements just as HTML
+const h1 = <h1>React JS</h1>;
+
+// define element attribute just as in HTML
+const a = <a href="#0">What a link!</a>;
+
+// multiline element are possible using parenthesis ()
+const ah1 = (
+  <a href="#0">
+    <h1>
+      What a big link!
+    </h1>
+  </a>
+);
+
+// a JSX expression must have exactly one outermost element
+// good habit is to have a div wrapping everything
+const blog = (
+  <div>
+    <h1>Main title</h1>
+    <p>Subtitle</p>
+  </div>
+);
+
+
+// render your HTML; first argument is the JSX element and the second points to the HTML where it will be rendered -> <div id="app"></diV>
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+
+// you can use variable of course
+const myElt = <h1>Render me!</h1>;
+ReactDOM.render(myElt, document.getElementById("app"));
+
+// or create it without JSX
+const myElt = React.createElement("h1", null, "Hello world");
+ReactDOM.render(myElt, document.getElementById("app"));
+
+
+// class property is special, contrary to HTML, JSX need it to be called className
+const myDiv = <div className="big"></div>;
+
+// self closing tags as well, they need the back slash
+const myImg = <img src="img.jpg" />;
+const myBr = <br />;
+
+// javascript into JSX thanks to curly braces {}
+const text = "The result of 2 + 3 is:";
+const myJS = <p>{text + " " 2 + 3}</p>;
+
+
+// create event listeners
+function myFunc() {
+  alert("Click on this image");
+}
+<img onClick={myFunc} />
+
+
+// if else are not possibile inside JSX
+// ternary operator
+const isTrue = <p>{1 === 1 ? "true" : "false"}</p>;
+
+// JSX conditionals; will render the HTML only if the JS is true
+const shouldBeEmpty = true;
+const myDiv = (
+  <div>
+    {!shouldBeEmpty && <p>Not Empty</p>} 
+  </div>
+);
+
+
+// map method and JSX; React understand it needs to make a list out of the array; use key attribute to make list item identifiable!
+const numbers = ["one", "two", "three"];
+const list = numbers.map((number, i) => <li key={"number_"+i}>{number}</li>);
+ReactDOM.render(
+  <ul>{list}</ul>, 
+  document.getElementById("app")
+);
+
+
+
+
+
+
+
