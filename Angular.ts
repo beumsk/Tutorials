e777b9900202046ec997d1128dc9013b7f2422a5
Rémy Beumier <beumieremy@gmail.com>
2021-04-13 09:47:25 +0200

    modules

--- Angular.ts
+++ Angular.ts
@@ -38,6 +38,29 @@
   }
 
 
+// MODULES
+
+  import { BrowserModule } from '@angular/platform-browser';
+  import { NgModule } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+
+  import { AppComponent } from './app.component';
+
+  @NgModule({
+    declarations: [
+      AppComponent,
+    ],
+    imports: [
+      CommonModule,
+      BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+
+  export class AppModule {}
+
+
 
 // EXPORT
 export class NameComponent {
