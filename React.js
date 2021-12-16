75d8fd42739c65c8168d0083a9ad19777ff982ae
Rémy Beumier <beumieremy@gmail.com>
2021-12-16 09:58:00 +0100

    react forms

--- React.js
+++ React.js
@@ -478,6 +478,28 @@ class Toggle extends React.Component {
 ReactDOM.render(<Toggle />, document.getElementById('app'));
 
 
+// react forms
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.state.input} onChange={this.handleChange} />
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
+
+
 // component lifecycle methods
 import React from 'react';
 export class Clock extends React.Component {
