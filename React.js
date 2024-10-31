0e19eeba69b710d34393ff8d44b3f5e1c54bf8c6
Rémy Beumier <beumieremy@gmail.com>
2024-10-31 17:10:50 +0100

    add useId

--- React.js
+++ React.js
@@ -285,7 +285,33 @@ const ChildComponent = () => {
 
 
 
+// ID HOOK
+// very useful to have unique IDs generated for similar elements
+import { useId } from 'react';
 
+function PasswordField() {
+  const passwordHintId = useId();
+  return (
+    <>
+      <label>
+        Password:
+        <input type="password" aria-describedby={passwordHintId} />
+      </label>
+      <p id={passwordHintId}>The password should contain at least 18 characters</p>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <h2>Choose password</h2>
+      <PasswordField />
+      <h2>Confirm password</h2>
+      <PasswordField />
+    </>
+  );
+}
 
 
 
